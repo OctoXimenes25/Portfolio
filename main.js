f33be54a16216e4d06d9ff3bb4b85e3901b75879
OctoXimenes25 <octavio.ximenes96@gmail.com>
2024-02-07 15:40:50 +0900

    add about

---
+++ main.js
@@ -0,0 +1,19 @@
+function toggleDetail(e){
+    // console.log(e)
+    const target = $(e.target)
+
+    if ($(target).hasClass("active")) {
+        $(target).html("More Info").removeClass("active")
+    }else{
+        $(target).html("Less Info").addClass("active")
+    }
+
+    
+
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+
+    $(detail).slideToggle()
+
+    // console.log($(item).children(".about-exp-item-detail"))
+}
